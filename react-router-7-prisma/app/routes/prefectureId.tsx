c81d41336ef84d53902a3126793922cb5c863f66
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-09-15 10:02:03 +0900

    各都道府県の詳細画面、遷移処理追加

--- react-router-7-prisma/app/routes/prefectureId.tsx
+++ react-router-7-prisma/app/routes/prefectureId.tsx
@@ -14,20 +14,22 @@ export async function loader() {
   return { prefectures: prefectures };
 }
 
-export default function Home({ loaderData }: Route.ComponentProps) {
+export default function prefecture({ loaderData }: Route.ComponentProps) {
   const { prefectures } = loaderData;
+  const prefectureId = window.location.pathname.split("/prefectures/")[1];
+
+  const foundPrefecture = prefectures.find(
+    (prefecture) => prefecture.id === prefectureId
+  );
+
+  const prefectureName = foundPrefecture ? foundPrefecture.name : undefined;
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] mt-16">
-        47都道府県旅行記録
+        {prefectureName}
       </h1>
-      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
-        {prefectures.map((prefecture) => (
-          <li key={prefecture.id} className="mb-2">
-            <Link to={`/prefectures/${prefecture.id}`}>{prefecture.name}</Link>
-          </li>
-        ))}
-      </ol>
+      <Link to={"/"}>戻る</Link>
     </div>
   );
 }
